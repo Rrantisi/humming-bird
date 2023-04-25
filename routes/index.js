e69adae9ee15399636fef9ecc96be4c1a4f8fdc2
mayramartins <mayramartins.in@gmail.com>
2023-04-25 07:14:19 -1000

    google callback route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const indexController = require('../controllers/index');
+const passport = require('passport');
 
-router.get('/', indexController.home)
+router.get('/', indexController.home);
+router.get('/auth/google', passport.authenticate(
+    'google',
+    {
+        scope: ['profile', 'email'],
+        prompt: "select_account"
+    }
+    
+));
+router.get('/oauth2callback', passport.authenticate(
+    'google',
+    {
+        successRedirect: '/posts',
+        failureRedirect: '/posts'
+    }
+))
 
 module.exports = router;
