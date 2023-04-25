dd52f2b245ea8a3b59ce84908bbaea2fe7b8260c
mayramartins <mayramartins.in@gmail.com>
2023-04-25 13:54:58 -1000

    wip: working on edit comment

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,40 +1,47 @@
-const Post = require('../models/post');
+const Post = require("../models/post");
 
 async function create(req, res) {
-    try {
-        const foundPost = await Post.findById(req.params.id);
+  try {
+    const foundPost = await Post.findById(req.params.id);
 
-        req.body.user = req.user._id;
-        req.body.userName = req.user.name;
-        req.body.userAvatar = req.user.avatar;
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    req.body.userAvatar = req.user.avatar;
 
-        foundPost.comments.push(req.body);
-        await foundPost.save();
-        res.redirect(`/posts/${foundPost._id}`);
-
-    } catch(error) {
-        console.error(error);
-        res.render('error', {
-            title: 'Something Went Wrong!'
-        })
-    }
-}
-  
-async function deleteComment(req, res) {
-    // Note the cool "dot" syntax to query on the property of a subdoc
-    // const post = await Post.findOne({ 'comments._id': req.params.id, 'comments.user': req.user._id });
-    const post = await Post.findById(req.params.id)
-    // Rogue user!
-    if (!post) return res.redirect('/posts');
-    // Remove the review using the remove method available on Mongoose arrays
-    post.comments.remove(req.params.commentId);
-    // Save the updated movie doc
-    await post.save();
-    // Redirect back to the movie's show view
-    res.redirect(`/posts/${post._id}`);
+    foundPost.comments.push(req.body);
+    await foundPost.save();
+    res.redirect(`/posts/${foundPost._id}`);
+  } catch (error) {
+    console.error(error);
+    res.render("error", {
+      title: "Something Went Wrong!",
+    });
   }
+}
+
+async function deleteComment(req, res) {
+  // Note the cool "dot" syntax to query on the property of a subdoc
+  // const post = await Post.findOne({ 'comments._id': req.params.id, 'comments.user': req.user._id });
+  const post = await Post.findById(req.params.id);
+  // Rogue user!
+  if (!post) return res.redirect("/posts");
+  // Remove the review using the remove method available on Mongoose arrays
+  post.comments.remove(req.params.commentId);
+  // Save the updated movie doc
+  await post.save();
+  // Redirect back to the movie's show view
+  res.redirect(`/posts/${post._id}`);
+}
+
+async function edit(req, res) {
+  const post = await Post.findById(req.params.id);
+  const comment = await post.find({ comment: comment._id });
+  if (!post) return res.redirect("/posts");
+  res.render("comments/edit", { title: "Edit Comment", post, comment });
+}
 
 module.exports = {
-    create,
-    delete: deleteComment
-}
+  create,
+  delete: deleteComment,
+  edit,
+};
--- routes/comments.js
+++ routes/comments.js
@@ -5,6 +5,9 @@ const commentsController = require('../controllers/comments');
 router.post('/posts/:id/comments', commentsController.create);
 
 // DELETE /reviews
-router.delete('/posts/:id/comments/:commentId', commentsController.delete);
+router.delete('/posts/:id/comments/:commentId', commentsController.delete)
+
+// EDIT / posts
+router.get('/posts/:id/comments/:commentId/edit', commentsController.edit);
 
 module.exports = router;
